88c933043ed003937ef9958cd847a39ce6b04b17
Waltair-Business-School <67534422+Waltair-Business-School@users.noreply.github.com>
2024-03-29 20:51:01 +0530

    countriessearch

---
+++ countriessearch/src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchinput, setSearchInput] = useState("");
+
+  const filteredData = countries.filter((country)=>
+  {return country.name.common.toLowerCase().includes(searchinput.toLowerCase());
+   })
+
+  const handleInput = (e)=>{
+    setSearchInput(e.target.value);
+    setCountries(filteredData);
+  }
+
+  const getCountriesData = async()=>{
+    try{
+      let res = await fetch(`https://restcountries.com/v3.1/all`);
+      let data = await res.json();
+      setCountries(data);
+      console.log(data);
+    }
+    catch(err){
+      console.log("Error fetching data: ", err);
+    }
+  }
+
+  useEffect(()=>{
+    getCountriesData()
+  },[]);
+
+
+  return (
+    <div>
+      <div className={styles.input}>
+      <input type="text" className={styles.field} 
+      value={searchinput}
+      placeholder='Search for countries...'
+      onChange={handleInput} />
+      </div>
+    <div className={styles.container}>
+        {countries.map((country)=>{
+        return(
+        <div key={country.cca3} className={styles.countryCard}>
+          <img className={styles.image} src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+          <h1 className={styles.name}>{country.name.common}</h1>
+        </div>
+        )
+      })}
+    </div>
+    </div>
+  );
+}
+
+export default App;
